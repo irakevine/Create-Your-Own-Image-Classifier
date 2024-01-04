d128ad0b94e99b85be7a847c7ac9b0283790657e
“@Kevine12” <ikevine2016@gmail.com>
2024-01-04 10:45:47 +0200

    adding data in file ex1

--- exercise.py
+++ exercise.py
@@ -2,8 +2,8 @@
 # jupyter nbconvert --to html 'Submission.ipynb'
 
 
-Sanity Checking
-Now that you can use a trained model for predictions, check to make sure it makes sense. Even if the testing accuracy is high, it's always good to check that there aren't obvious bugs. Use matplotlib to plot the probabilities for the top 5 classes as a bar graph, along with the input image. It should look like this:
+# Sanity Checking
+# Now that you can use a trained model for predictions, check to make sure it makes sense. Even if the testing accuracy is high, it's always good to check that there aren't obvious bugs. Use matplotlib to plot the probabilities for the top 5 classes as a bar graph, along with the input image. It should look like this:
 
 
 # You can convert from the class integer encoding to actual flower names with the cat_to_name.json file (should have been loaded earlier in the notebook). To show a PyTorch tensor as an image, use the imshow function defined above.
